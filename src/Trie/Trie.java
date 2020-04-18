afae6cc6b09e9b61b9350c161737927718bcf17d
edieye <e.y.96@hotmail.com>
2020-04-17 17:32:14 -0700

    Trie Number1

--- src/Trie/Trie.java
+++ src/Trie/Trie.java
@@ -7,6 +7,8 @@ package Trie;
 import java.util.HashMap;
 
 public class Trie {
+
+    /** **/
     /** Initialize your data structure here. */
     Node top;
     public Trie() {
@@ -17,19 +19,49 @@ public class Trie {
     public void insert(String word) {
         Node curr = top;
         for (int i = 0; i < word.length() ;i++) {
-            if ()
+            HashMap<Character, Node> map = curr.map;
+            if (!map.containsKey(word.charAt(i))) {
+                Node n = new Node();
+                map.put(word.charAt(i), n);
+            }
+            if (i == word.length() - 1) {
+                Node isEndNode = map.get(word.charAt(i));
+                isEndNode.isEnd = true;
+                map.put(word.charAt(i), isEndNode);
+            }
+            curr = map.get(word.charAt(i));
         }
 
     }
 
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-
+        Node curr = top;
+        for (int i = 0; i < word.length(); i++){
+            HashMap<Character, Node> map = curr.map;
+            if (!map.containsKey(word.charAt(i))) {
+                return false;
+            }
+            if (i == word.length() -1) {
+                Node isEndNode = map.get(word.charAt(i));
+                return isEndNode.isEnd;
+            }
+            curr = map.get(word.charAt(i));
+        }
+        return false;
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-
+        Node curr = top;
+        for (int i = 0; i < prefix.length(); i++) {
+            HashMap<Character, Node> map = curr.map;
+            if (!map.containsKey(prefix.charAt(i))) {
+                return false;
+            }
+            curr = map.get(prefix.charAt(i));
+        }
+        return true;
     }
 
     private class Node {
